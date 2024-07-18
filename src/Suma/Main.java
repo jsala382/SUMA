4f97c170a4f0a9722c9b715f19235fbf28dd7151
Jorge Salazar <naruto_011990@hotmail.com>
2024-07-18 18:09:28 -0500

    Version multiplicacion

--- src/Suma/Main.java
+++ src/Suma/Main.java
@@ -6,18 +6,20 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner  input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int num1;
         int num2;
         int suma;
         System.out.println("Ingrese un numero");
-        num1= input.nextInt();
+        num1 = input.nextInt();
         System.out.println("Ingrese otro numeto");
-        num2=input.nextInt();
-        suma=num1+num2;
-        int resta=num1-num2;
-        System.out.println("la suma de num1 + num2 es igual a "+ suma);
-        System.out.println("la diferencia de num1 - num2 es igual a "+ resta);
-        
+        num2 = input.nextInt();
+        suma = num1 + num2;
+        int resta = num1 - num2;
+        int multiplicacion = num1 * num2;
+        System.out.println("la suma de num1 + num2 es igual a " + suma);
+        System.out.println("la diferencia de num1 - num2 es igual a " + resta);
+        System.out.println("El producto  de num1 * num2 es igual a " + multiplicacion);
+
     }
 }
